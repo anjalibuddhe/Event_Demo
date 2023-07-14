520350f2e6b85307de1989423aba2d97411f491b
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-14 14:00:41 +0530

    Add project files.

---
+++ EventDemo/Bank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    public delegate void EventHandler();
+    public class Bank
+    {
+        public event EventHandler ZeroBalance;
+        public event EventHandler InsufficientBalance;
+        public event EventHandler LowBalance;
+        public double Balance;
+        public Bank(double Balance)
+        {
+            this.Balance = Balance;
+        }
+
+        public void DebitAmount(double Amount)
+        {
+            if (Amount > Balance)
+            {
+                InsufficientBalance();
+            }
+            else if (Amount < Balance)
+            {
+                Balance -= Amount;
+                if (Balance == 0)
+                {
+                    ZeroBalance();
+                }
+                else if (Balance < 3000)
+                {
+                    LowBalance();
+                }
+
+
+            }
+        }
+        public void CreditAmount(double Amount)
+        {
+            Balance = Balance + Amount;
+            CheckBalance();
+        }
+        public string CheckBalance()
+        {
+            return $"{Balance}";
+
+        }
+    }
+}
---
+++ EventDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    internal class Program
+    {
+        static void ZeroBalance()
+        {
+            Console.WriteLine("You have Zero Balance In your account");
+        }
+        static void InsufficientBalance()
+        {
+            Console.WriteLine("You have Insufficient Balance");
+        }
+        static void LowBalance()
+        {
+            Console.WriteLine("You have Low Balance");
+        }
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank(10000);
+            bank.ZeroBalance += new EventHandler(ZeroBalance);
+            bank.InsufficientBalance += new EventHandler(InsufficientBalance);
+            bank.LowBalance += new EventHandler(LowBalance);
+
+            bank.CreditAmount(100);
+            bank.DebitAmount(200);
+            bank.CheckBalance();
+            bank.CheckBalance();
+            Console.WriteLine(bank.CheckBalance());
+
+        }
+    }
+ }
+
